277cf19b1d5b7cb396dd9085de0dc16a28a368f5
timo <timo-faes@live.be>
2024-01-12 11:32:28 +0100

    speed

--- components/layout.js
+++ components/layout.js
@@ -3,13 +3,15 @@ import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const name = 'Your Name';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
   return (
+    
     <div className={styles.container}>
+    <SpeedInsights/>  
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
