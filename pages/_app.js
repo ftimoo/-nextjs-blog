ae2d592378bef7dd26daa0d5fe9e9ba91c762b5e
timo <timo-faes@live.be>
2024-01-12 11:45:28 +0100

    speed

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,8 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
-  return
-  <> <Component {...pageProps} />
-  <Analytics />
-  <SpeedInsights/>  
-
-  </>
+  return<> <Component {...pageProps} />
+      <SpeedInsights/>  
+      <Analytics />
+  </>;
 }
